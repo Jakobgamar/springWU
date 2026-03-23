17eea54ac952ae9b8b11daf53417fa3b8df10696
Jakobgamar <Jakob.finndin@elev.ga.ntig.se>
2026-03-23 14:40:18 +0100

    haburgarmeny searchbar och produkter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 
+function toggleMenu() {
+    const menu = document.getElementById('hamburgerMenu');
+    menu.classList.toggle('open');
+}
+
+document.addEventListener('click', function(e) {
+    const wrapper = document.querySelector('.hamburger-wrapper');
+    if (!wrapper.contains(e.target)) {
+        document.getElementById('hamburgerMenu').classList.remove('open');
+    }
+});
